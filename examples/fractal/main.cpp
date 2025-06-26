c6109233dbcc4de20b7e1e736a3917bc5f9f8776
Vinbu <vincent.14.damian@gmail.com>
2025-06-26 13:01:15 -0400

    Mpi aplicated to fern fractal code.

--- examples/fractal/main.cpp
+++ examples/fractal/main.cpp
@@ -1,161 +1,91 @@
+#include <mpi.h>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <climits>
 
-#include <opencv2/opencv.hpp>
-
 using namespace std;
 using namespace cv;
 
-Point2f f1(Point2f p) {
-    Point2f retval;
-    retval.x = 0;
-    retval.y = 0.16*p.y;
+const int WIDTH = 1920;
+const int HEIGHT = 1080;
+const int SCALE = 150;
+const int TOTAL_ITER = 1000000;
 
-    return retval;
-}
+Point2f f1(Point2f p) { return Point2f(0, 0.16f * p.y); }
 
 Point2f f2(Point2f p) {
-    Point2f retval;
-    retval.x = 0.2*p.x - 0.26*p.y;
-    retval.y = 0.23*p.x + 0.22*p.y + 1.6;
-
-    return retval;
+    return Point2f(0.2f * p.x - 0.26f * p.y,
+                   0.23f * p.x + 0.22f * p.y + 1.6f);
 }
 
 Point2f f3(Point2f p) {
-    Point2f retval;
-    retval.x = -0.15*p.x + 0.28*p.y;
-    retval.y = 0.26*p.x + 0.24*p.y + 0.44;
-
-    return retval;
+    return Point2f(-0.15f * p.x + 0.28f * p.y,
+                   0.26f * p.x + 0.24f * p.y + 0.44f);
 }
 
 Point2f f4(Point2f p) {
-    Point2f retval;
-    retval.x = 0.85*p.x + 0.04*p.y;
-    retval.y = -0.04*p.x + 0.85*p.y + 1.6;
-
-    return retval;
+    return Point2f(0.85f * p.x + 0.04f * p.y,
+                   -0.04f * p.x + 0.85f * p.y + 1.6f);
 }
-// Point2f f1(Point2f p) {
-//     Point2f retval;
-//     retval.x = 0.5f*p.x;
-//     retval.y = 0.5f*p.y;
 
-//     return retval;
-// }
-
-// Point2f f2(Point2f p) {
-//     Point2f retval;
-//     retval.x = 0.5f*p.x + 0.5f;
-//     retval.y = 0.5f*p.y;
-
-//     return retval;
-// }
-
-// Point2f f3(Point2f p) {
-//     Point2f retval;
-//     retval.x = 0.5f*p.x + 0.25f;
-//     retval.y = 0.5f*p.y + sqrt(3.f)/4;
-
-//     return retval;
-// }
-
-// Point2i determineWindowSize(vector<Point2f> points) {
-//     Point2i maximum = Point2i(INT_MIN, INT_MIN);
-//     for (int i = 0; i < points.size(); ++i) {
-//         int x = points.at(i).x;
-//         int y = points.at(i).y;
-
-//         maximum.x = x > maximum.x ? x : maximum.x;
-//         maximum.y = x > maximum.x ? x : maximum.x;
-//     }
-//     return maximum;
-// }
-
-// void drawFractal(float xRule, float yRule, vector<Point2f> points, int iterations = 50000) {
-//     Point2f lastPosition = Point2f(0, 0);
-//     const int dieWalls = points.size();
-//     Point2i upperRightCorner = determineWindowSize(points);
-
-//     Mat image = Mat(upperRightCorner.x, upperRightCorner.y, CV_8UC1, Scalar::all(0));
-
-//     for (int i = 0; i < iterations; ++i) {
-//         int rnd = rand()%dieWalls;
-//         Point2f pointTo = points.at(rnd);
-//         Point2f diff = pointTo - lastPosition;
-//         lastPosition.x += xRule*diff.x;
-//         lastPosition.y += yRule*diff.y;
-
-//         image.at<uchar>(lastPosition.y, lastPosition.x) = 255;
-//     }
-//     imshow("Fractal", image);
-//     waitKey();
-// }
-
-// void drawSierpinskiIFS(int iterations = 50000) {
-//     Point2f lastPosition = Point2f(0, 0);
-//     const int dieWalls = 3;
-
-//     Mat image = Mat(1080, 1920, CV_8UC1, Scalar::all(0));
-
-//     for (int i = 0; i < iterations; ++i) {
-//         int rnd = rand()%dieWalls;
-
-//         if (rnd==0) {
-//             lastPosition = f1(lastPosition);
-//         }
-//         if (rnd==1) {
-//             lastPosition = f2(lastPosition);
-//         }
-//         if (rnd==2) {
-//             lastPosition = f3(lastPosition);
-//         }
-
-//         image.at<uchar>(lastPosition.y*1000, lastPosition.x*1000) = 255;
-//     }
-//     imshow("Sierpinski IFS", image);
-//     waitKey();
-// }
-
-void drawBarnsleayFernIFS(int iterations) {
-    Point2f lastPosition = Point2f(0, 0);
+void generateFern(Mat& image, int iterations, int seed) {
+    Point2f pos(0, 0);
     const int dieWalls = 100;
-
-    Mat image = Mat(1080, 1920, CV_8UC1, Scalar::all(0));
+    srand(seed);
 
     for (int i = 0; i < iterations; ++i) {
-        int rnd = rand()%dieWalls;
+        int rnd = rand() % dieWalls;
+        if (rnd < 1)
+            pos = f1(pos);
+        else if (rnd < 8)
+            pos = f2(pos);
+        else if (rnd < 15)
+            pos = f3(pos);
+        else
+            pos = f4(pos);
 
-        if (rnd<=1) {
-            lastPosition = f1(lastPosition);
-        }
-        else if (rnd<=8) {
-            lastPosition = f2(lastPosition);
-        }
-        else if (rnd<=15) {
-            lastPosition = f3(lastPosition);
-        }
-        else {
-            lastPosition = f4(lastPosition);
-        }
+        int x = static_cast<int>(pos.x * SCALE + WIDTH / 2);
+        int y = static_cast<int>(HEIGHT - pos.y * SCALE);
 
-        image.at<uchar>((lastPosition.x+3)*150, lastPosition.y*150) = 255;
+        if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+            image.at<uchar>(y, x) = 255;
+        }
     }
-    imshow("Barnsley Fern IFS", image);
-    waitKey();
 }
 
-int main() {
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
 
-    // vector<Point2f> points;
-    // points.push_back(Point2f(0, 0));
-    // points.push_back(Point2f(1000, 0));
-    // points.push_back(Point2f(500, 866));
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // drawFractal(0.5, 0.5, points);
-    drawBarnsleayFernIFS(1000000);
+    int local_iter = TOTAL_ITER / size;
+
+    // Imagen local de cada nodo
+    Mat local_image = Mat::zeros(HEIGHT, WIDTH, CV_8UC1);
+    generateFern(local_image, local_iter, 1234 + rank);
+
+    // Imagen final solo en el proceso 0
+    Mat global_image;
+    if (rank == 0) {
+        global_image = Mat::zeros(HEIGHT, WIDTH, CV_8UC1);
+    }
+
+    // Reunir las imágenes usando reducción por máximo (para binario)
+    MPI_Reduce(local_image.data, 
+                (rank == 0 ? global_image.data : nullptr),
+                WIDTH * HEIGHT, MPI_UNSIGNED_CHAR,
+                MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        imshow("Fern", global_image);
+        waitKey();
+    }
+
+    MPI_Finalize();
     return 0;
 }
+
