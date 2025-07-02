cee59b3d9e2f61311102cba912cad94301fa7faa
Dante Quezada <129406602+DalexQ@users.noreply.github.com>
2025-07-02 17:00:41 -0400

    Update script_ring2.sh

--- examples/msg_ring/script_ring2.sh
+++ examples/msg_ring/script_ring2.sh
@@ -4,9 +4,9 @@ scp ring2.cpp mpi@node04:~/uss-patagon-cluster/examples/msg_ring
 
 mpic++ ring2.cpp -o ring2 
 echo "node01 ok"
-ssh node02 mpic++ ~/uss-patagon-cluster/examples/msg_ring/ring_2.cpp -o ~/uss-patagon-cluster/examples/msg_ring/ring2
+ssh node02 mpic++ ~/uss-patagon-cluster/examples/msg_ring/ring2.cpp -o ~/uss-patagon-cluster/examples/msg_ring/ring2
 echo "node02 ok"
-ssh node03 mpic++ ~/uss-patagon-cluster/examples/msg_ring/ring_2.cpp -o ~/uss-patagon-cluster/examples/msg_ring/ring2
+ssh node03 mpic++ ~/uss-patagon-cluster/examples/msg_ring/ring2.cpp -o ~/uss-patagon-cluster/examples/msg_ring/ring2
 echo "node03 ok"
-ssh node04 mpic++ ~/uss-patagon-cluster/examples/msg_ring/ring_2.cpp -o ~/uss-patagon-cluster/examples/msg_ring/ring2
+ssh node04 mpic++ ~/uss-patagon-cluster/examples/msg_ring/ring2.cpp -o ~/uss-patagon-cluster/examples/msg_ring/ring2
 echo "node04 ok"
