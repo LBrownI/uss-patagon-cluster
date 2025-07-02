18fd51b1239ee636aa43a1d6a3ef11a041ae14be
vicentexte <vicentexte@gmail.com>
2025-07-02 14:27:14 -0400

    Update RaidMPI.cpp

--- examples/raidMPI/RaidMPI.cpp
+++ examples/raidMPI/RaidMPI.cpp
@@ -1,13 +1,14 @@
 #include <mpi.h>
 #include <iostream>
 #include <vector>
+#include <unistd.h>
 
 using namespace std;
 
 const int BLOCK_SIZE = 4;
 
-void xorBlocks(vector<int> &result, const vector<int> &a, const vector<int> &b) {
-    for (int i = 0; i < BLOCK_SIZE; i++) {
+void xorBlocks(vector<int>& result, const vector<int>& a, const vector<int>& b) {
+    for (int i = 0; i < BLOCK_SIZE; ++i) {
         result[i] = a[i] ^ b[i];
     }
 }
@@ -18,68 +19,75 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
 
     vector<int> data(BLOCK_SIZE);
 
     if (rank == 0) {
-        // Datos originales
-        vector<int> d1 = {10, 20, 30, 40};
-        vector<int> d2 = {50, 60, 70, 80};
-        vector<int> d3 = {90,100,110,120};
-        vector<int> parity(BLOCK_SIZE);
+        // Crear bloques de datos para cada nodo (excepto el nodo 0 que es maestro)
+        int dataNodes = size - 1;
+        vector<vector<int>> blocks(dataNodes, vector<int>(BLOCK_SIZE));
 
-        char hostname[256]
-        gethostname(hostname,sizeof(hostname))
+        // Asignar datos arbitrarios
+        for (int i = 0; i < dataNodes; ++i) {
+            for (int j = 0; j < BLOCK_SIZE; ++j) {
+                blocks[i][j] = (i + 1) * 10 + j;
+            }
+        }
 
-        // Calcular paridad: P = d1 ^ d2 ^ d3
-        vector<int> temp(BLOCK_SIZE);
-        xorBlocks(temp, d1, d2);
-        xorBlocks(parity, temp, d3);
+        // Calcular la paridad con XOR de todos los bloques
+        vector<int> parity(BLOCK_SIZE, 0);
+        for (const auto& block : blocks) {
+            xorBlocks(parity, parity, block);
+        }
 
-        // Enviar datos y paridad
-        MPI_Send(d1.data(), BLOCK_SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        MPI_Send(d2.data(), BLOCK_SIZE, MPI_INT, 2, 0, MPI_COMM_WORLD);
-        MPI_Send(d3.data(), BLOCK_SIZE, MPI_INT, 3, 0, MPI_COMM_WORLD);
-        MPI_Send(parity.data(), BLOCK_SIZE, MPI_INT, 3, 1, MPI_COMM_WORLD);
+        // Enviar cada bloque a su nodo correspondiente
+        for (int i = 0; i < dataNodes; ++i) {
+            MPI_Send(blocks[i].data(), BLOCK_SIZE, MPI_INT, i + 1, 0, MPI_COMM_WORLD);
+        }
+
+        // Enviar paridad al último nodo
+        MPI_Send(parity.data(), BLOCK_SIZE, MPI_INT, size - 1, 1, MPI_COMM_WORLD);
     }
 
-    // Recibir datos en los nodos
-    if (rank == 1 || rank == 2 || rank == 3) {
+    // Todos los nodos diferentes del maestro (rank != 0)
+    if (rank > 0) {
         MPI_Recv(data.data(), BLOCK_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        cout << "Nodo " << hostname << " recibió datos: ";
+        cout << "Nodo " << hostname << " (rank " << rank << ") recibió datos: ";
         for (int val : data) cout << val << " ";
         cout << endl;
     }
 
-    // Simular recuperación en el nodo 0 si falla el nodo 2
+    // Simular recuperación en nodo 0 si falla un nodo arbitrario (por ejemplo, rank 2)
+    int failed_rank = 2;
+
     if (rank == 0) {
-        cout << "\nSimulando falla del nodo 2 (rank 2)..." << endl;
+        cout << "\nSimulando falla del nodo " << failed_rank << "..." << endl;
 
-        vector<int> d1(BLOCK_SIZE), d3(BLOCK_SIZE), parity(BLOCK_SIZE), recovered(BLOCK_SIZE);
+        vector<int> recovered(BLOCK_SIZE, 0);
+        vector<int> parity(BLOCK_SIZE, 0);
 
-        // Recibir de nodos 1 y 3
-        MPI_Recv(d1.data(), BLOCK_SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(d3.data(), BLOCK_SIZE, MPI_INT, 3, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(parity.data(), BLOCK_SIZE, MPI_INT, 3, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        // Recibir la paridad del último nodo
+        MPI_Recv(parity.data(), BLOCK_SIZE, MPI_INT, size - 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        // Recuperar d2: d2 = parity ^ d1 ^ d3
-        vector<int> temp(BLOCK_SIZE);
-        xorBlocks(temp, parity, d1);
-        xorBlocks(recovered, temp, d3);
+        // Recuperar datos haciendo XOR de todos menos el fallado
+        for (int i = 1; i < size; ++i) {
+            if (i == failed_rank) continue;
 
-        cout << "Datos recuperados del nodo 2: ";
+            vector<int> temp(BLOCK_SIZE);
+            MPI_Recv(temp.data(), BLOCK_SIZE, MPI_INT, i, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            xorBlocks(recovered, recovered, temp);
+        }
+
+        xorBlocks(recovered, recovered, parity);
+
+        cout << "Datos recuperados del nodo " << failed_rank << ": ";
         for (int val : recovered) cout << val << " ";
         cout << endl;
-    } else {
-        // Enviar bloques a nodo 0 para recuperación
-        if (rank == 1)
-            MPI_Send(data.data(), BLOCK_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        if (rank == 3) {
-            MPI_Send(data.data(), BLOCK_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
-            vector<int> parity(BLOCK_SIZE);
-            MPI_Recv(parity.data(), BLOCK_SIZE, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(parity.data(), BLOCK_SIZE, MPI_INT, 0, 1, MPI_COMM_WORLD);
-        }
+    } else if (rank != failed_rank) {
+        // Enviar datos a nodo 0 para recuperación
+        MPI_Send(data.data(), BLOCK_SIZE, MPI_INT, 0, 2, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
