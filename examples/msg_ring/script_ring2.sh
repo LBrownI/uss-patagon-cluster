d90ca36d56cfd590cdc09588536c0fcde48e3fca
Dante Quezada <129406602+DalexQ@users.noreply.github.com>
2025-07-02 16:57:56 -0400

    Update script_ring2.sh

--- examples/msg_ring/script_ring2.sh
+++ examples/msg_ring/script_ring2.sh
@@ -1,6 +1,6 @@
-scp main.cpp mpi@node02:~/uss-patagon-cluster/examples/msg_ring
-scp main.cpp mpi@node03:~/uss-patagon-cluster/examples/msg_ring
-scp main.cpp mpi@node04:~/uss-patagon-cluster/examples/msg_ring
+scp ring2.cpp mpi@node02:~/uss-patagon-cluster/examples/msg_ring
+scp ring2.cpp mpi@node03:~/uss-patagon-cluster/examples/msg_ring
+scp ring2.cpp mpi@node04:~/uss-patagon-cluster/examples/msg_ring
 
 mpic++ main.cpp -o main 
 echo "node01 ok"
